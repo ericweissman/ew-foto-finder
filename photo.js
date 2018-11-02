88fac4c1ceab4adeeea72f40c49f8a89bc627743
Eric Weissman <eric.weissman21@gmail.com>
2018-11-02 09:42:41 -0600

    Start photo class cosntructor

---
+++ photo.js
@@ -0,0 +1,9 @@
+class Photo {
+  constructor(id, title, caption){
+    this.id = id || Date.now();
+    this.title = title;
+    this.caption = caption;
+    this.file = ??
+    this.favorite = false;
+  }
+}
