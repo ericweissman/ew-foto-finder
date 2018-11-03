aed2b2ac88f51391ec7e4c37ccc34746fbebad2f
Eric Weissman <eric.weissman21@gmail.com>
2018-11-03 12:29:43 -0600

    Save to Local Storage and reload on page

--- main.js
+++ main.js
@@ -1,49 +1,91 @@
 // EVENT LISTENERS
-// document.querySelector('.input-area').addEventListener('click', testAlert);
-// document.querySelector('.favorites').addEventListener('click', favAlert)
+document.querySelector('.search-bar').addEventListener('keyup', testAlert);
 
-document.querySelector('.add'), addEventListener('click', addToAlbumButton);
+document.querySelector('.favorites').addEventListener('click', testAlert);
 
+document.querySelector('.small-btns'), addEventListener('click', addPhotoCardInfo);
 
-
-// function testAlert(){
-//   alert('Hi');
-// }
-
-// function favAlert() {
-//   alert('Favs');
-// }
-
-// function addAlert() {
-//   alert("add to album");
-// }
-
-// Add Card To DOM
-function buildPhotoCardAsObject(obj) {
-  var photo = new Photo(obj.id, obj.title, obj.caption, obj.file);
-
-  addCardWith(photo);
-}
-
-function addCardWith(photo) {
-  var newPhotoCard = document.createElement('article');
-
-  newPhotoCard.className = 'image-card';
-  buildPhotoCardInfo(photo);
-  document.querySelector('.image-card-area').prepend(newPhotoCard);
-}
-
-
-function addToAlbumButton(event) {
+function testAlert(event){
   event.preventDefault();
-  var title = document.querySelector('#title-input').value;
-  var caption = document.querySelector('#caption-input').value;
+  console.log('Hi');
+}
 
-  if (title && caption) {
-    var photo = new Photo(title, caption);
-    buildPhotoCardAsObject(photo);
-    // photo.saveToStorage();
+var photoAlbum = [];
+
+window.onload = pullCardsFromStorage;
+
+
+function addPhotoCardInfo(event) {
+  var titleText = document.querySelector('#title-input').value;
+  var captionText = document.querySelector('#caption-input').value
+  
+  if (event.target.classList.contains('add') && titleText && captionText){
+    var photo = new Photo(titleText, captionText);
+    buildPhotoCardObject(photo);
+    photo.saveToStorage();
+    addToAlbumArray(photo);
+    event.preventDefault();
   }
 }
 
+// Add Photo Cards to Array
+function addToAlbumArray(photoObject){
+  photoAlbum.push(photoObject);
+}
 
+// ADD CARDS TO Page
+
+// Photo Card Creation
+function buildPhotoCardObject(object) {
+  var photo = new Photo(object.title, object.caption, object.id, object.file)
+
+  addCardWith(photo)
+};
+
+function addCardWith(photo) {
+  var newPhotoCard = document.createElement('section');
+ 
+  newPhotoCard.className = 'image-card';
+  photo.photoCardInfo(newPhotoCard);
+  document.querySelector('.image-card-area').prepend(newPhotoCard);
+  clearInputs();
+}
+
+function clearInputs() {
+  document.querySelector("#title-input").value = null;
+  document.querySelector("#caption-input").value = null;
+}
+
+
+
+// ADDING REMOVING FROM LOCAL STORAGE 
+
+
+
+function addAllCardsFromStorage() {
+  var keys = Object.keys(localStorage);
+  var targets = document.querySelector('.image-card-area');
+  // remove all from page
+  while (targets.firstChild) {
+    targets.removeChild(targets.firstChild);
+  }
+  // Adding all cards  
+  keys.forEach(function (key) {
+    var parsedStorageIdeas = JSON.parse(localStorage.getItem(key));
+    buildPhotoCardObject(parsedStorageIdeas);
+    photoAlbum.push(photoObject);
+  })
+}
+
+
+function pullCardsFromStorage() {
+  var keys = Object.keys(localStorage);
+  var numKeys = keys.length;
+  var index = numKeys > 10 ? numKeys - 10 : 0;
+
+  for (index; index < keys.length; index++) {
+    var parsedStorageIdeas = JSON.parse(localStorage.getItem(keys[index]));
+    buildPhotoCardObject(parsedStorageIdeas);
+    photoAlbum.push(parsedStorageIdeas);
+  }
+}
--- photo.js
+++ photo.js
@@ -1,18 +1,17 @@
 class Photo {
-  constructor(id, title, caption, file){
-    this.id = id || Date.now();
+  constructor(title, caption, id, file){
     this.title = title;
     this.caption = caption;
+    this.id = id || Date.now();
     this.file = file;
     this.favorite = false;
   }
-}
 
   photoCardInfo(photoCard) {
-    photoCard.innerHTML =
-    `<article class="image-card" data-name="${this.id}">
-      <h2 class="card-title" >${this.title}</h2>
-      <img class="card-img" src="${this.file}" alt="${this.id} photo">
+    photoCard.id = this.id
+    photoCard.innerHTML = `<article data-name="${this.id}">
+      <h2 class="card-title">${this.title}</h2>
+      <img class="card-img" src="http://www.cpr.org/sites/default/files/images/rr11.jpg" alt="${this.id} photo">
       <p>${this.caption}</p>
       <section class="card-bottom">
         <button class="card-btns delete"></button>
@@ -20,6 +19,18 @@ class Photo {
       </section>
     </article>`;
   }
+
+  saveToStorage() {
+    localStorage.setItem(this.id, JSON.stringify(this));
+  }
 }
+
+
+
+//   deleteFromStorage(event) {
+//     localStorage.removeItem(event.target.parentElement.id)
+//   };
+// }
+
 
 
